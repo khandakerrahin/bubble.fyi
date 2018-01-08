df7dccb752063aa3c916868abdc90715ec46213a
Ahmed <Ahmed@Spider-Wasif>
2018-01-08 13:37:16 +0600

    commit 20180108

---
+++ BubbleFyi/ejbModule/org/bubble/fyi/DBOperations/SMSSender.java
@@ -0,0 +1,136 @@
+
+package org.bubble.fyi.DBOperations;
+
+import java.sql.SQLException;
+import org.bubble.fyi.DataSources.BubbleFyiDS;
+import org.bubble.fyi.Engine.JsonDecoder;
+import org.bubble.fyi.Logs.LogWriter;
+
+/**
+ * @author hafiz
+ *
+ */
+public class SMSSender {
+	BubbleFyiDS bubbleDS;
+	/**
+	 * 
+	 */
+	public SMSSender() {
+		bubbleDS = new BubbleFyiDS();
+	}
+	/**
+	 * If msisdn starts with 0, prepends 88.
+	 * If msisdn starts with 880 or any other number, returns the String
+	 * @param msisdn
+	 * @return msisdn of the format 8801xx
+	 */
+	private String msisdnNormalize(String msisdn) {
+		if(msisdn.startsWith("0")) {
+			msisdn="88"+msisdn;
+		}
+		return msisdn;
+	}
+	//new customer not yet approved 0
+	//approved customer 1 , admin 5 
+	public String getUserType(String userId) {
+		String retval="-1";
+		String sql="select flag from tbl_users where id=?";
+			try {
+				bubbleDS.prepareStatement(sql);
+				bubbleDS.getPreparedStatement().setString(1, userId);
+				bubbleDS.executeQuery();
+				if(bubbleDS.getResultSet().next()) {
+					retval=bubbleDS.getResultSet().getString(1);
+				}else {
+					retval="1:User not found";
+				}
+				bubbleDS.closeResultSet();
+				bubbleDS.closePreparedStatement();
+			} catch (SQLException e) {
+				retval="-2";
+				LogWriter.LOGGER.severe("getUserType(): "+e.getMessage());
+			}finally{
+				if(bubbleDS.getConnection() != null){
+					try {
+						bubbleDS.getConnection().close();
+					} catch (SQLException e) {
+						retval="-3";
+						LogWriter.LOGGER.severe(e.getMessage());
+					}
+				}      
+			}
+		
+		return retval;
+	}
+	/**
+	 * 
+	 * @param jsonDecoder msisdn,smsText,id
+	 * @return 0:Successfully Inserted
+	 * <br>1:User with the email address exists
+	 * <br>2:Inserting  details failed
+	 * <br>11:Inserting  credentials failed
+	 * <br>E:JSON string invalid
+	 * <br>-1:Default Error Code
+	 * <br>-2:SQLException
+	 * <br>-3:General Exception
+	 * <br>-4:SQLException while closing connection
+	 */
+	public String insertToSender(JsonDecoder jsonDecoder) {
+		String errorCode="-1";//default errorCode	
+		//TODO check if eligible to send sms.
+		String userStatus=getUserType(jsonDecoder.getJsonObject().getString("id"));
+		if(userStatus.equals("1") || userStatus.equals("5")) {
+			
+		String sqlInsert="INSERT INTO smsinfo"
+				+ " (userid,message,bparty,source) "
+				+ "VALUES (?, ?, ?,'bubble')";
+
+		try {
+			try {
+				bubbleDS.prepareStatement(sqlInsert);
+				//	(custodian_name,organisation_name,username,password,email,phone, address,postcode,city);
+				bubbleDS.getPreparedStatement().setString(1, jsonDecoder.getJsonObject().getString("id"));					
+				bubbleDS.getPreparedStatement().setString(2, jsonDecoder.getJsonObject().getString("smsText"));
+				bubbleDS.getPreparedStatement().setString(3, this.msisdnNormalize(jsonDecoder.getEString("msisdn")));
+				bubbleDS.execute();
+				errorCode="0:Successfully Inserted";
+			}catch(SQLException e) {
+				errorCode="11:Inserting  credentials failed";
+				LogWriter.LOGGER.severe(e.getMessage());
+				bubbleDS.closePreparedStatement();
+				//new UserDBOperations().deleteUsersEntry(userId); //only deletes from users table
+			}catch(Exception de) {
+				errorCode="-10: other exception";
+				LogWriter.LOGGER.severe(de.getMessage());
+				//de.printStackTrace();
+				LogWriter.LOGGER.info("other Exception:"+de.getMessage());
+			}
+			if(bubbleDS.getConnection() != null) bubbleDS.closePreparedStatement();
+
+		}catch(SQLException e){
+			errorCode= "-2";
+			LogWriter.LOGGER.severe(e.getMessage());
+		}catch(Exception e){
+			errorCode= "-3";
+			LogWriter.LOGGER.severe(e.getMessage());
+			e.printStackTrace();
+		}finally{
+			if(bubbleDS.getConnection() != null){
+				try {
+					//					fsDS.getConnection().setAutoCommit(true);
+					bubbleDS.getConnection().close();
+				} catch (SQLException e) {
+					errorCode="-4";
+					LogWriter.LOGGER.severe(e.getMessage());
+				}
+			}      
+
+		}
+	}else {
+		errorCode="-6:Sent sms not allowed";
+	}
+		return errorCode;
+	}
+
+
+}
---
+++ BubbleFyi/ejbModule/org/bubble/fyi/Engine/SMSProcessor.java
@@ -0,0 +1,50 @@
+package org.bubble.fyi.Engine;
+
+import org.bubble.fyi.DBOperations.SMSSender;
+
+/**
+ * @author wasif
+ *
+ */
+public class SMSProcessor {
+
+	/**
+	 * 
+	 */
+	public SMSProcessor() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @json bparty,userid,message
+	 * @example { "schoolName":"Skola 1","email":"spiderco@sdxb.com","phone":"8801912345678","password":"spidercom","custodianName":"SpiderCom","address":"10A Dhanmondi","city":"Dhaka","postcode":"1209"}
+	 * @requestParameters action=registerSchool&message=jsonDecoder
+	 * @param message
+	 * @param messageBody
+	 * @return 0:Successfully Inserted
+	 * <br>1:User with the email address or phone number exists
+	 * <br>2:Inserting organization details failed
+	 * <br>11:Inserting user credentials failed
+	 * <br>E:JSON string invalid
+	 * <br>-1:Default Error Code
+	 * <br>-2:SQLException
+	 * <br>-3:General Exception
+	 * <br>-4:SQLException while closing connection
+	 */
+	public String processSendSMS(String message, String messageBody) {
+		String retval="E";
+		JsonDecoder SMSInfo;
+		if(messageBody.isEmpty()) {
+			SMSInfo=new JsonDecoder(message);
+		}else {
+			SMSInfo=new JsonDecoder(messageBody);
+		}
+		SMSSender sendSMS=new SMSSender();
+		if(SMSInfo.getErrorCode().equals("0")) {
+			retval=sendSMS.insertToSender(SMSInfo);
+		}else{
+			//error decoding json
+			retval="E:JSON string invalid";
+		}
+		return retval;
+	}
+}
